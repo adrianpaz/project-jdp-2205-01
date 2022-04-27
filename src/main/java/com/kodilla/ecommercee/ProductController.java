c643bde7ecb660a9e595488e67b956a0f0b3d6aa
Jarek Warowny <jwarowny99@gmail.com>
2022-04-28 00:49:00 +0200

    Implementacja endpointow

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,29 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
 public class ProductController {
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    public ProductDto getProduct() {
+        return new ProductDto();
+    }
+
+    public void createProduct() {
+
+    }
+
+    public ProductDto updateProduct() {
+        return new ProductDto();
+    }
+
+    public void deleteProduct() {
+
+    }
 }
