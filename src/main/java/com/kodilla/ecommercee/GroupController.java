99fb4aee89dfcd363ad584ed70b72087606e133a
WiktorIdziak <Witusaki@gmail.com>
2022-04-27 16:28:19 +0200

    GroupController - dummy endpoints

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,29 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    @GetMapping()
+    public String getGroup() {
+        return "Group";
+    }
+
+    @DeleteMapping()
+    public String deleteTask() {
+        return "Group has been deleted";
+    }
+
+    @PutMapping
+    public String updateTask() {
+        return "Group has been update";
+    }
+
+    @PostMapping()
+    public String createTask() {
+        return "Group has been added";
+    }
 }
