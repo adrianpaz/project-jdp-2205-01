5a8258dabdf2d2170502c5e780ac9b0e4a164658
Grzegorz Wrobel <grzegorz.j.wrobel@gmail.com>
2022-04-27 06:37:57 +0200

    UserController - new annotations added

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,9 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/users")
 public class UserController {
 }
