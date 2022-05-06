832431341fdd4faf189c6ab05097b5c97c38af21
WiktorIdziak <Witusaki@gmail.com>
2022-05-06 19:07:01 +0200

    GroupDto - added listOfProductsId

--- src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
@@ -1,7 +1,9 @@
 package com.kodilla.ecommercee.dto;
 
+import java.util.List;
+
 public class GroupDto {
     private Long id;
     private String name;
-
+    private List<Long> listOfProductsId;
 }
