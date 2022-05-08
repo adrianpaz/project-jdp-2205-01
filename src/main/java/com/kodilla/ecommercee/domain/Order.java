2ba1529619d171455c1591679d715e42d1cafb5b
Jarek Warowny <jwarowny99@gmail.com>
2022-05-08 16:04:14 +0200

    zmiany

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,12 +1,12 @@
 package com.kodilla.ecommercee.domain;
 
 import com.kodilla.ecommercee.dto.CartItemDto;
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,8 +25,5 @@ public final class Order {
     private Long id;
 
     private Long userId;
-
-    private List<CartItemDto>
-
 
 }
