e346acf77bc44acb07994ac56ee934fb53b71216
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-05-09 22:02:08 +0200

    task-JDP220501-15

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,26 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+}
--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -2,14 +2,46 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.*;
 
-@Data
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "USERS")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "ID", unique = true)
     private Long id;
+
+    @Column(name = "USERNAME")
     private String username;
+
+    @Column(name = "STATUS")
     private String status;
+
+    @Column(name = "USER_KEY")
     private Long userKey;
 
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Order> orders = new ArrayList<>();
+
+    @OneToMany(
+            targetEntity = Cart.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Cart> carts = new ArrayList<>();
 }
