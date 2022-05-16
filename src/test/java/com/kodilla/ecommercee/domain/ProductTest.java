8f886010c42f855a2ae19a17be6167f6f9bad0f7
WiktorIdziak <Witusaki@gmail.com>
2022-05-16 22:31:36 +0200

    ProductTest - CRUD finish

--- src/test/java/com/kodilla/ecommercee/domain/ProductTest.java
+++ src/test/java/com/kodilla/ecommercee/domain/ProductTest.java
@@ -9,10 +9,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
+@Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class ProductTest {
@@ -51,10 +53,10 @@ public class ProductTest {
         Optional<Product> test = productRepository.findById(productEntity.getId());
 
         Assert.assertTrue(test.isPresent());
-        Assert.assertEquals(test.get().getName(),"CreateProductTest");
-        Assert.assertEquals(test.get().getDescription(),"Create Test");
-        Assert.assertEquals(test.get().getPrice(),new BigDecimal("24.99"));
-        Assert.assertEquals(test.get().getGroup().getName(),"ProductTestGroup");
+        Assert.assertEquals("CreateProductTest",test.get().getName());
+        Assert.assertEquals("Create Test",test.get().getDescription());
+        Assert.assertEquals(new BigDecimal("24.99"),test.get().getPrice());
+        Assert.assertEquals("ProductTestGroup",test.get().getGroup().getName());
     }
 
     @Test
@@ -72,7 +74,37 @@ public class ProductTest {
 
 
 
-        Assert.assertTrue(productRepository.existsById(productEntity.getId()));
+        Assert.assertFalse(productRepository.existsById(productEntity.getId()));
+    }
+
+    @Test
+    public void shouldUpdateProduct(){
+
+        String name = "ProductTest";
+        String description = "Test";
+        BigDecimal price = new BigDecimal("24.99");
+        Group group = new Group("ProductTestGroup");
+        Group groupEntity = groupRepository.save(group);
+
+        String updatedName = "UpdatedProductTest";
+        String updatedDescription = "Updated Test";
+        BigDecimal updatedPrice = new BigDecimal("23.99");
+        Group updatedGroup = new Group("UpdatedProductTestGroup");
+
+        groupEntity = groupRepository.save(updatedGroup);
+
+
+        Product productEntity = productRepository.save(new Product(name,description,price,group));
+        productRepository.deleteById(productEntity.getId());
+
+        productEntity = productRepository.save(new Product(updatedName,updatedDescription,updatedPrice,updatedGroup));
+        Optional<Product> test = productRepository.findById(productEntity.getId());
+
+        Assert.assertTrue(test.isPresent());
+        Assert.assertEquals("UpdatedProductTest",test.get().getName());
+        Assert.assertEquals("Updated Test",test.get().getDescription());
+        Assert.assertEquals(new BigDecimal("23.99"),test.get().getPrice());
+        Assert.assertEquals("UpdatedProductTestGroup",test.get().getGroup().getName());
     }
 
 }
