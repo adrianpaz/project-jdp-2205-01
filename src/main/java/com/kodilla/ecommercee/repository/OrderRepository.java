2d457b9b96d40babc65fec068ad31ade576512c0
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-05-16 23:15:06 +0200

    task-JDP220501-16

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
+    @Override
+    List<Order> findAll();
 }
